3d84d3b9d584f1f0f2d7ff413b01517057274eaf
CherMiTTT <chermi20000@yandex.ru>
2025-01-19 16:37:22 +0300

    Simple stack on linked list + test

--- stack.h
+++ stack.h
@@ -1,11 +1,34 @@
 #ifndef DATA_STRUCTURES_STACK_H
 #define DATA_STRUCTURES_STACK_H
 
+#include <memory>
+
 template <class T>
-class stack {
+class stack_linked_list {
+
+    struct node {
+        T data;
+        std::unique_ptr<node> next;
+    };
+
 public:
+    stack_linked_list() : top(nullptr) {}
+
+    T pop() {
+        T tmp = top->data;
+        top = std::move(top->next);
+        return tmp;
+    }
+
+    void push(T item) {
+        auto n = std::make_unique<node>(item, move(top));
+        top = move(n);
+    }
+
+    bool empty() {return top == nullptr;}
 
 private:
+    std::unique_ptr<node> top;
 };
 
 #endif //DATA_STRUCTURES_STACK_H
--- tests/stack_test.cpp
+++ tests/stack_test.cpp
@@ -1,7 +1,18 @@
 #include "../stack.h"
 #include <gtest/gtest.h>
 
-TEST(HelloTest, BasicAssert) {
+TEST(StackLLTest, BasicAssert) {
+    auto st = new stack_linked_list<int>();
+    for(int i = 0; i < 10; i++) {
+        st->push(i);
+    }
+
+    for(int i = 9; i >= 0; i--) {
+        EXPECT_FALSE(st->empty());
+        EXPECT_EQ(st->pop(), i);
+    }
+    EXPECT_TRUE(st->empty());
+
     EXPECT_STRNE("hello", "world");
     EXPECT_EQ(7 * 6, 42);
 }
